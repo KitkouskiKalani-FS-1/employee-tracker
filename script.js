2aead26766d317de518d2b469c9d34c6a5192c59
Kalani Kitkouski <KMKitkouski@student.fullsail.edu>
2021-10-17 14:38:54 -0500

    Added classes with contructors and unfinished methods

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 class Employee{
     constructor(){
-        
+        this.name = "";
+        this.age = 0;
+        this.annualSalary = 0;
+    }
+}
+class PartTime extends Employee{
+    constructor(){
+        super();
+        this.payRate = 0;
+        this.employeeType = "";
+        this.hours = 0;
+    }
+    calculatePay(){
+
+    }
+}
+
+class FullTime extends Employee{
+    constructor(){
+        super();
+        this.payRate = 0;
+        this.employeeType = "";
+    }
+    calculatePay(){
+
     }
 }
 class Main{
