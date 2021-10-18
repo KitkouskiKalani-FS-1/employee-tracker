307f86230416e83b35964a1df989ef78fcb56c29
Kalani Kitkouski <KMKitkouski@student.fullsail.edu>
2021-10-18 12:23:22 -0500

    Added switch statement and allowed for user input

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ class FullTime extends Employee{
         super(name, age);
         this.payRate = payRate;
         this.employeeType = "Full Time";
+        this.hours = 40;
         this.annualSalary = this.calculatePay();
     }
     calculatePay(){
@@ -32,13 +33,36 @@ class FullTime extends Employee{
 class Main{
     constructor(){
         this.employees = [];
-        this.displayEmployees();
         this.addEmployee("Kalani", 27, 40, 20);
         this.addEmployee("Brandon", 45, 25, 12);
         this.addEmployee("Pamela", 42, 15, 50);
+        this.displayEmployees();
+        this.showMenu();
     }
     showMenu(){
-
+        let again = true;
+        while(again){
+            let userChoice = parseInt(prompt("Main Menu\n1. Add Employee\n2. Remove Employee\n3. Edit Employee\n4. Display Employees"));
+            switch (userChoice) {
+                case 1:
+                    let userEmployee = prompt("Add employee name, age, hours/wk, pay rate [separate each by a comma]").split(",");
+                    console.log(userEmployee);
+                    this.addEmployee(userEmployee[0],userEmployee[1],userEmployee[2],userEmployee[3]);
+                    break;
+                case 2:
+            
+                    break;
+                case 3:
+            
+                    break;
+                case 4:
+                    this.displayEmployees();
+                    break;
+                default:
+                    again = true;
+                    break;
+            }
+        }
     }
     addEmployee(name,age,hours,payRate){
         if(hours<40){
@@ -57,7 +81,13 @@ class Main{
 
     }
     displayEmployees(){
-        console.log(this.employees)
+        let id = 1;
+        console.log("My Employees");        
+        console.log(`ID\tName\tSalary\thrs\tpay\tFT/PT`);
+        this.employees.forEach(employee=>{
+            console.log(`${id}\t${employee.name}\t${employee.annualSalary}\t\t${employee.hours}\t${employee.payRate}\t${employee.employeeType}`);
+            id++;
+        })
     }
 }
 (()=>{
